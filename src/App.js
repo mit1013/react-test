616e705377975381baca2f3415a6b079b2aaaab0
mit1013 <74683455+mit1013@users.noreply.github.com>
2020-11-24 14:01:00 +0000

    fixed merge conflicts

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
 import React from "react";
-import Form from "./Form";
 import "./styles.css";
 
 export default function App() {
   return (
     <div className="App">
-      <Form />
+      <div className="jumbotron text-center">
+        <h1>My Bootstrap Page</h1>
+        <p>Resize this responsive page to see the effect!</p>
+      </div>
+
+      <div className="container">
+        <div className="row">
+          <div className="col-sm-4">
+            <h3>Column 1</h3>
+            <p>Lorem ipsum dolor..</p>
+          </div>
+          <div className="col-sm-4">
+            <h3>Column 2</h3>
+            <p>Lorem ipsum dolor..</p>
+          </div>
+          <div className="col-sm-4">
+            <h3>Column 3</h3>
+            <p>Lorem ipsum dolor..</p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
